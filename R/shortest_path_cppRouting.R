489f0117d8b2592b13dda8a20d5b3ec5afc1d955
sindre-haugen <sindre.haugen@ssb.no>
2022-10-17 09:57:01 +0200

    TR fix

--- R/shortest_path_cppRouting.R
+++ R/shortest_path_cppRouting.R
@@ -2,11 +2,11 @@
 #' Shortest path (cppRouting)
 #'
 #' The function `shortest_path_cppRouting` can be used to calculate the shortest path (either in minutes or meters) between two or more nodes in the Norwegian road network. The function also works with vectors with multiple from and to node ID’s.
-#' Before the function can be used, the road network must be converted to a cppRouting object that is called `graph_cppRouting_FT_MINUTES` or `graph_cppRouting_LENGTH`. This can be done with the function [GISSB::vegnett_to_R()].
+#' Before the function can be used, the road network must be converted to a cppRouting object that is called `graph_cppRouting_minutes` or `graph_cppRouting_meters`. This can be done with the function [GISSB::vegnett_to_R()].
 #'
 #' @param from_node_ID Numeric vector with one more from node ID’s.
 #' @param to_node_ID Numeric vector with one more to node ID’s.
-#' @param unit Character vector with "FT_MINUTES" to calculate the shortest path in minutes or "LENGTH" for the shortest path in meters.
+#' @param unit Character vector with "minutes" to calculate the shortest path in minutes or "meters" for the shortest path in meters.
 #' @param dist Character vector that specifies if all the shortest paths between all the supplied from and to nodes are returned, or if only the minimum/maximum value per from node ID is returned.
 #'
 #' @returns Object (data.frame) with how many minutes or meters the shortest path is between the supplied from and to node ID’s.
@@ -16,11 +16,11 @@
 #' \dontrun{
 #' distance_min <- shortest_path_cppRouting(26956,
 #'                                        210373,
-#'                                        unit = "FT_MINUTES")
+#'                                        unit = "minutes")
 #'
 #' distance_meter <- shortest_path_cppRouting(26956,
 #'                                          210373,
-#'                                          unit = "LENGTH")
+#'                                          unit = "meters")
 #'                                          }
 #' @encoding UTF-8
 #'
@@ -28,16 +28,18 @@
 
 shortest_path_cppRouting <- function(from_node_ID,
                                 to_node_ID,
-                                unit = "FT_MINUTES",
+                                unit = "minutes",
                                 dist = "all") {
 
-  if (unit == "FT_MINUTES") {
-    graph_cppRouting <- graph_cppRouting_FT_MINUTES
+  if (unit == "minutes") {
+    graph_cppRouting <- graph_cppRouting_minutes
   }
 
-  if (unit == "LENGTH") {
-    graph_cppRouting <- graph_cppRouting_LENGTH
+  if (unit == "meters") {
+    graph_cppRouting <- graph_cppRouting_meters
   }
+
+  # OBS: legg til feilmelding dersom graph_cppRouting_minutes eller graph_cppRouting_meters ikke finnes?
 
 
   dists <- cppRouting::get_distance_matrix(graph_cppRouting,
@@ -49,19 +51,19 @@ shortest_path_cppRouting <- function(from_node_ID,
   dists2 <- tibble::rownames_to_column(dists2, "from_nodeID")
   dists2_long <- reshape2::melt(dists2, id.vars = "from_nodeID",
                                 variable.name = "to_nodeID",
-                                value.name = "length")
+                                value.name = "meters")
   dists2_long$to_nodeID <- gsub("X", "", dists2_long$to_nodeID)
 
   if (dist == "min") {
     dists2_long <- dists2_long %>%
       dplyr::group_by(from_nodeID) %>%
-      dplyr::slice(which.min(length))
+      dplyr::slice(which.min(meters))
   }
 
   if (dist == "max") {
     dists2_long <- dists2_long %>%
       dplyr::group_by(from_nodeID) %>%
-      dplyr::slice(which.max(length))
+      dplyr::slice(which.max(meters))
   }
 
   dists2_long$from_nodeID <- as.integer(dists2_long$from_nodeID)
